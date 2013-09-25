d326af734856a56df70fa84e7c2e999df2420de3
Ryan Buzzell <ryanhypenlee@gmail.com>
2013-09-25 12:38:25 -0400

    Removed massive collection of if statements

--- colorcodes.cpp
+++ colorcodes.cpp
@@ -7,7 +7,7 @@
 #include <string>
 using namespace std;
 
-int bandValue(string);
+int bandValue(string, bool);
 int main ()
 {
 	//Strings related to user input
@@ -34,174 +34,26 @@ int main ()
 	cout << "Enter the color of the fourth band: " << endl;
 	cin >> color4;
 	cin.ignore (1000, '\n');
+    band1 = bandValue(color1, false);
+    band2 = bandValue(color2, false);
+    band3 = bandValue(color3, true);
+	//The Math Section
 
-    //To define values for each band
-	do
-	{
-		if (color1 == "black")
-		{
-			band1 = 0;
-		}
-		else if (color1 == "brown")
-			{
-			band1 = 1;
-			}
-			else if (color1 == "red")
-				{
-				band1 = 2;
-				}
-				else if (color1 == "orange")
-					{
-					band1 = 3;
-					}
-					else if (color1 =="yellow")
-						{
-						band1 = 4;
-						}
-						else if (color1 == "green")
-							{
-							band1 = 5;
-							}
-							else if (color1 == "blue")
-								{
-								band1 = 6;
-								}
-								else if (color1 == "violet")
-									{
-									band1 = 7;
-									}
-									else if (color1 == "grey")
-										{
-										band1 = 8;
-										}
-										else if (color1 == "white")
-											{
-											band1 = 9;
-											}
-											else
-											{
-												band1 = 1234; //sets a failure condition
-												cout << "ERROR! must be in all lowercase. Also the purple one has to be entered as violet" << endl;
-												cout << "Enter the colors on the color band in order. Program will accept the following format 'color'" << endl;
-												cout <<"Enter the color of the first band: " << endl;
-												cin >> color1;
-											}
-	}
-	while (band1 = 1234);
+	total = band1 * band3 + band2 * (band3 /10); //To calculate the total resistance of the resistor
 
-//	do
-	{
-		if (color2 == "black")
-		{
-			band2 = 0;
-		}
-		else if (color2 == "brown")
-			{
-			band2 = 1;
-			}
-			else if (color2 == "red")
-				{
-				band2 = 2;
-				}
-				else if (color2 == "orange")
-					{
-					band2 = 3;
-					}
-					else if (color2 =="yellow")
-						{
-						band2 = 4;
-						}
-						else if (color2 == "green")
-							{
-							band2 = 5;
-							}
-							else if (color2 == "blue")
-								{
-								band2 = 6;
-								}
-								else if (color2 == "violet")
-									{
-									band2 = 7;
-									}
-									else if (color2 == "grey")
-										{
-										band2 = 8;
-										}
-										else if (color2 == "white")
-											{
-											band2 = 9;
-											}
-											else
-												{
-												band2 = 1234;
-												cout << "ERROR! must be in all lowercase. Also the purple one has to be entered as violet" << endl;
-												}
-	}
-//	while (band2 = 1234);
-//	do
-	{
-		if (color3 == "black")
-		{
-			band3 = 1;
-		}
-		else if (color3 == "brown")
-			{
-			band3 = 10;
-			}
-			else if (color3 == "red")
-				{
-				band3 = 100;
-				}
-				else if (color3 == "orange")
-					{
-					band3 = 1000;
-					}
-					else if (color3 =="yellow")
-						{
-						band3 = 10000;
-						}
-						else if (color3 == "green")
-							{
-							band3 = 10000;
-							}
-							else if (color3 == "blue")
-								{
-								band3 = 1000000;
-								}
-								else if (color3 == "violet")
-									{
-									band3 = 1000000;
-									}
-									else if (color3 == "grey")
-										{
-										band3 = 100000000;
-										}
-										else if (color3 == "white")
-											{
-											band3 = 1000000000;
-											}
-											else
-												{
-												band3 = 1234;
-												cout << "ERROR! must be in all lowercase. Also the purple one has to be entered as violet" << endl;
-												}
-	}
-		//The Math Section
-
-		total = band1 * band3 + band2 * (band3 /10); //To calculate the total resistance of the resistor
-
-		//The output section
-		cout << fixed << setprecision (0) << setw (13) <<endl;
-		cout << "The corresponding number for band 1 is: " << band1 <<endl;
-		cout << "The corresponding number for band 2 is: " << band2 <<endl;
-		cout << "The multiplier for band 3 is: " << band3 <<endl;
-		cout << "The total resistance of the resistor is: " << total <<endl;
+	//The output section
+	cout << fixed << setprecision (0) << setw (13) <<endl;
+	cout << "The corresponding number for band 1 is: " << band1 <<endl;
+	cout << "The corresponding number for band 2 is: " << band2 <<endl;
+	cout << "The multiplier for band 3 is: " << band3 <<endl;
+	cout << "The total resistance of the resistor is: " << total <<endl;
 	return 0;
 }
 
-int bandValue(string color){
+int bandValue(string color, bool thirdBand){
 
     int value =  0;
+    if (thirdBand == false){
     if (color == "black")
         value = 0;
         else if(color == "brown")
@@ -222,5 +74,28 @@ int bandValue(string color){
                                         value == 8;
                                         else if(color == "white")
                                             value == 9;
+    }
+    else {
+        if(color == "black")
+            value = 1;
+        else if(color == "brown")
+            value = 10;
+        else if(color == "red")
+            value = 100;
+        else if(color == "orange")
+            value = 1000;
+        else if(color == "yellow")
+            value = 10000;
+        else if(color == "green")
+            value == 100000;
+        else if(color == "blue")
+            value = 1000000;
+        else if(color == "violet" || color == "purple")
+            value = 10000000;
+        else if(color == "gray" || color == "grey")
+            value = 100000000;
+        else if(color == "white")
+            value = 1000000000;
+    }
     return value;
 }
